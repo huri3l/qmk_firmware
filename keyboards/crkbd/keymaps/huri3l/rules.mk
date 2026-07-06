71b1d74216aedd13c6ba2da2829f1de953b7c02d
Huriel Lopes <huriel-lopes@outlook.com>
2026-07-06 19:08:22 -0300

    feat: disable RGB and add Luna

---
+++ keyboards/crkbd/keymaps/huri3l/rules.mk
@@ -0,0 +1,7 @@
+# Fully disable RGB for this keymap, regardless of what the keyboard enables.
+# Overrides the crkbd/rev1 default that turns rgblight on.
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = no
+
+# Luna keyboard pet on the left OLED needs typing-speed tracking.
+WPM_ENABLE = yes
